5796cf977566c57550f89fa019e1dfa792a2d031
Jesper Josefsson <jesper.josefsson@gmail.com>
2013-05-13 11:03:43 +0200

    Add move operations

--- ruby-planner/spec/world_spec.rb
+++ ruby-planner/spec/world_spec.rb
@@ -2,6 +2,7 @@ require File.dirname(__FILE__) + "/../world.rb"
 
 describe World do
   let(:world) { World.new }
+
   it "should have a world and blocks" do
     world.should respond_to(:world)
     world.should respond_to(:blocks)
@@ -9,20 +10,74 @@ describe World do
 
   describe "color" do
     it "should be able to find blocks of a certain color" do
-      world.where(color: "red").should eq(["c", "h", "j", "l"])
-      world.where(color: "blue").should eq(["a", "g", "m"])
-      world.where(color: "white").should eq(["b", "e"])
-      world.where(color: "yellow").should eq(["i", "k"])
-      world.where(color: "green").should eq(["d"])
+      world.with_properties(color: "red").should eq(["c", "h", "j", "l"])
+      world.with_properties(color: "blue").should eq(["a", "g", "m"])
+      world.with_properties(color: "white").should eq(["b", "e"])
+      world.with_properties(color: "yellow").should eq(["i", "k"])
+      world.with_properties(color: "green").should eq(["d"])
     end
 
     it "should be able to find all blocks of arbitrary color" do
-      world.where(color: "_").should eq(("a".."m").to_a)
+      world.with_properties(color: "_").should eq(("a".."m").to_a)
     end
   end
 
   it "should be able to combine queries" do
-    world.where(color: "green", form: "pyramid").should eq(["d"])
-    world.where(color: "red", form: "box", size: "large").should eq(["j"])
+    world.with_properties(color: "green", form: "pyramid").should eq(["d"])
+    world.with_properties(color: "red", form: "box", size: "large").should eq(["j"])
+  end
+
+  describe "position" do
+    it "should give the right position" do
+      world.position_of("a").should eq([1,0])
+      world.position_of("b").should eq([1,1])
+    end
+
+    it "should give the right coordinate for a leftof" do
+      world.leftof_position("a").should eq([0,0])
+    end
+
+    it "should give the right coordinate for a rightof" do 
+      world.rightof_position("a").should eq([2,2])
+    end
+  end
+
+  describe "moving" do
+    it "does ontop correctly" do
+      world.ontop("a", "b")
+      world.get_column(1).should eq(["b", "a"])
+    end
+
+    it "does above correctly" do
+      world.above("c", "a")
+      world.get_column(1).should eq(["a", "b", "c"])
+      world.get_column(2).should eq(["d"])
+    end
+
+    it "does leftof correctly" do
+      world.leftof("a", "b")
+      world.get_column(0).should eq(["a"])
+      world.get_column(1).should eq(["b"])
+    end
+
+    it "does rightof correctly" do
+      world.rightof("a", "b")
+      world.get_column(1).should eq(["b"])
+      world.get_column(2).should eq(["c", "d", "a"])
+    end
+
+    it "does beside correctly" do
+      world.beside("a", "m")
+      world.get_column(1).should eq(["b"])
+      world.get_column(8).should eq(["a"])
+    end
+  end
+
+  describe "inserting" do
+    it "should insert blocks correctly" do
+      world.insert_block_at_position("x", [1,0])
+      world.position_of("x").should eq([1,0])
+      world.get_column(1).should eq ["x", "a", "b"]
+    end
   end
 end
--- ruby-planner/world.rb
+++ ruby-planner/world.rb
@@ -1,6 +1,8 @@
 class World
-  attr_reader :world, :blocks
-  def initialize(world = nil)
+  PREPOSITIONS = ["leftof", "under", "rightof", "ontop", "beside", "above"]
+  attr_reader :world, :blocks, :grabber
+  def initialize(world = nil, grabber = nil)
+    @grabber = grabber
     @world = world || [[], ["a","b"], ["c","d"], [], ["e","f","g","h","i"], [], [], ["j","k"], [], ["l","m"]];
     @blocks = {
     "a" =>  { "form" => "rectangle", "size" => "tall",   "color" => "blue",   "width" => 0.50, "height" => 1.00 },
@@ -19,7 +21,7 @@ class World
     }
   end
 
-  def where(args)
+  def with_properties(args)
     @blocks.select do |name, block|
       match = true
       args.each do |key, value|
@@ -27,5 +29,113 @@ class World
       end
       match
     end.keys
+  end
+
+  def position_of(block_name)
+    column_index = column_index_of(block_name)
+    return nil unless column_index
+    height = get_column(column_index).index {|row| row == block_name}
+    [column_index, height]
+  end
+
+  def get_column(column_index)
+    return @world[column_index]
+  end
+
+  def column_index_of(block_name)
+    column_index = @world.index {|col| col.include? block_name}
+  end
+
+  def with_preposition(preposition, block_name)
+    if PREPOSITIONS.include? preposition
+      self.send(preposition, block_name)
+    end
+  end
+
+  def remove_block_from_column(block_name, column_index) 
+    get_column(column_index).reject! {|block| block == block_name}
+    return block_name
+  end
+
+  def remove_block(block_name)
+    column_index = column_index_of(block_name)
+    remove_block_from_column(block_name, column_index)
+  end
+
+  def top_of_column(column_index)
+    column = get_column(column_index)
+    if column
+      return column.size
+    else
+      return nil
+    end
+  end
+
+  def put_on_top_of_column(block, column_index)
+    get_column(column_index).push(block)
+  end
+
+  # Horizontal operations
+
+  def leftof_position(block_name)
+    calculate_column(position_of(block_name), -1)
+  end
+  
+  def rightof_position(block_name)
+    calculate_column(position_of(block_name), 1)
+  end
+
+  def beside_position(block_name)
+    leftof_position(block_name) || rightof_position(block_name)
+  end
+
+  def calculate_column(origin, diff)
+    new_column = origin[0] + diff
+    position = [new_column, top_of_column(new_column)]
+    if sane(position)
+      return position
+    else
+      return nil
+    end
+  end
+
+  ["leftof", "rightof", "beside"].each do |position|
+    define_method position do |subject, target|
+      remove_block(subject)
+      target_position = self.send("#{position}_position", target)
+      insert_block_at_position(subject, target_position)
+    end
+  end
+
+
+  # Vertical operations
+
+  def ontop(subject, target)
+    remove_block(subject)
+    target_block_position  = position_of(target)
+    column = target_block_position[0]
+    row = target_block_position[1] + 1
+    insert_block_at_position(subject, [column, row])
+  end
+
+  def above(subject, target)
+    remove_block(subject)
+    column_index = column_index_of(target)
+    put_on_top_of_column(subject, column_index)
+  end
+
+  def insert_block_at_position(block, position)
+    if sane(position)
+      column_index = position[0]
+      row_index = position[1]
+      column = get_column(column_index)
+      column.insert(row_index, block)
+    end
+  end
+
+  # Sanity checks
+
+  def sane(position)
+    position[0] >= 0 && position[1] >= 0 && position[0] < @world.size
   end
 end
