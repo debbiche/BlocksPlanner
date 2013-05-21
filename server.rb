6557d9c6dc8303a7f2021426bbc9c34e10b705b2
Jesper Josefsson <jesper.josefsson@gmail.com>
2013-05-21 10:03:19 +0200

    Add comments

--- server.rb
+++ server.rb
@@ -2,9 +2,21 @@ require 'sinatra'
 require File.dirname(__FILE__) + "/ruby-planner/parser"
 require File.dirname(__FILE__) + "/ruby-planner/world"
 
+#
+# BLOCKSPLANNER WEB SERVER
+# 
+# This is the Web Server component of the BlockPlanner application
+# It handles parser and planner requests from the web client,
+# relaying these to appropriate components as needed.
+#
+# @author Jesper Josefsson
+#
+
+# These routes 'simulate' the origin CGI scripts, in order minimize changes to the client code
 get '/parser.cgi' do
   content_type("text/plain")
   input = params[:input]
+  # Run the Python parser script
   output = %x(./parser.cgi "#{input}")
 end
 
@@ -12,13 +24,9 @@ get '/planner.cgi' do
   content_type("text/plain")
   holding, trees, world = params[:holding], params[:trees], params[:world]
   initial_world = World.new(world, holding)
-  # # Worlds are mutable (sorry...) :P
-  # final_world   = World.new(world, holding)
-  tree_wrapper  = Parser.parse(trees.split("\n").first)
-  puts tree_wrapper.tree.command.to_facts(initial_world)
+  tree_wrapper  = Parser.parse(trees.split("\n").first) # We only use the first syntax tree, if there are many
   facts = tree_wrapper.tree.command.to_facts(initial_world)
   command = %Q(runhaskell PlannerFacts "#{initial_world.encode_world}" "#{facts}" "#{initial_world.encode_blocks}")
-  puts command
   plan = `#{command}`
   plan.gsub(/\"/, "").split(";").join("\n")
 end
